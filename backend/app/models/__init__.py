50405c662379f497350650f1aef25d29787da620
tzyi <cabie8399@gmail.com>
2025-07-22 08:18:14 +0800

    [Del] 刪除code

--- backend/app/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .feed import Feed
-from .article import Article
-from .user_article import UserArticle
-from .category import Category
-
-__all__ = ['User', 'Feed', 'Article', 'UserArticle', 'Category']
